98d322ced6c503aa3629ee7e93f10ede6dbeffce
Mehdi Hoseini <mehdi.hoseinipajooh@gmail.com>
2018-02-19 17:05:37 +0330

    :recycle: .env files instead of .js

--- services/database.js
+++ services/database.js
@@ -3,8 +3,6 @@ const debug = require('debug')('development');
 
 module.exports = {
 	connect(mongoose) {
-		console.log(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/lastscene`);
-		console.log('mongodb://mehdi:5BB4nlLLGXS4EYFWNhSR6CumGAOfgqb16@ds237748.mlab.com:37748/lastscene');
 		if (process.env.MONGODB_USERNAME) {
 			mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/lastscene`, {
 				autoReconnect: true
