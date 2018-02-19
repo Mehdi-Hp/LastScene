9713f602309d209c2a8b0c2735f3914e22efff78
Mehdi Hoseini <mehdi.hoseinipajooh@gmail.com>
2018-02-19 17:01:05 +0330

    :construction: setting env variables

--- services/database.js
+++ services/database.js
@@ -3,7 +3,8 @@ const debug = require('debug')('development');
 
 module.exports = {
 	connect(mongoose) {
-		console.log(process.env);
+		console.log(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/lastscene`);
+		console.log('mongodb://mehdi:5BB4nlLLGXS4EYFWNhSR6CumGAOfgqb16@ds237748.mlab.com:37748/lastscene');
 		if (process.env.MONGODB_USERNAME) {
 			mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/lastscene`, {
 				autoReconnect: true
