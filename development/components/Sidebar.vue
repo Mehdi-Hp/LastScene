50ca843d2ca35bfd4b7098b93f4bde31acd9a368
Mehdi Hoseini <mehdi.hoseinipajooh@gmail.com>
2018-04-20 17:32:04 +0430

    :bug: show either name or username

--- development/components/Sidebar.vue
+++ development/components/Sidebar.vue
@@ -8,7 +8,7 @@
 					alt=""
 				/>
 			</div>
-			<span class="o-lggdin-user__name">{{ info.name }}</span>
+			<span class="o-lggdin-user__name">{{ info.name || info.username }}</span>
 			<button class="o-lggdin-user__settings-btn">
 				<icon-setting></icon-setting>
 			</button>
