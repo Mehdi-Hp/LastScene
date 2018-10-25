415af3d3fc179fdf502a355b976e0666c2ff8406
Mehdi Hoseini <mehdi.hoseinipajooh@gmail.com>
2018-10-25 19:14:52 +0330

    :lipstick: :bug: fix button text alignment

---
+++ public/development/components/1_atoms/AppButton.vue
@@ -0,0 +1,95 @@
+<template>
+	<button
+		v-ripple="'rgba(255, 255, 255, 0.10)'"
+		:class="`button--${mode}`"
+		class="button"
+	>
+		<icon
+			v-if="icon"
+			:name="icon"
+			:class="`button__icon--${mode}`"
+			class="button__icon"
+		/>
+		<span
+			v-if="text"
+			class="button__text"
+		>
+			{{ text }}
+		</span>
+	</button>
+</template>
+
+<script>
+export default {
+	name: 'AppButton',
+	props: {
+		mode: {
+			type: String,
+			required: true
+		},
+		text: {
+			type: String,
+			required: false,
+			default: null
+		},
+		icon: {
+			type: String,
+			required: false,
+			default: null
+		}
+	},
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style scoped lang="scss">
+.button {
+	box: horizontal;
+	padding: 0.85em 1.25em;
+	border-radius: 10em;
+	font-weight: 500;
+	font-family: $secondary-typeface;
+	transition: background-color 0.15s;
+	font-size: 0.9em;
+
+	&--normal {
+		background-color: $white-2;
+		color: $black-1;
+
+		&:hocus {
+			background-color: $white-3;
+		}
+	}
+
+	&--ghost {
+		background-color: transparent;
+		color: $primary-color;
+		font-size: 1em;
+	}
+
+	&--primary {
+		background-color: $red;
+		color: tint(mix($red, $gold), 40%);
+
+		&:hocus {
+			background-color: shade($red, 10%);
+		}
+	}
+
+	&__icon {
+		margin-right: 0.5em;
+		size: 1.2em;
+
+		&--primary {
+		}
+		&--ghost {
+		}
+	}
+
+	&__text {
+		transform: translateY(0.1em);
+	}
+}
+</style>
