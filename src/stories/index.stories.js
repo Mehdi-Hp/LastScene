54ae442f5fa96b86e61fe067b428bfa01424d2f4
Mehdi <m.hosseinipajooh@alibaba.ir>
2020-01-03 17:56:56 +0330

    :tada: Initial setup storybook

---
+++ src/stories/index.stories.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import MyButton from '../components/MyButton.vue';
+
+storiesOf('Button', module)
+    .add('with text', () => {
+        return {
+            components: { MyButton },
+            template: '<my-button @click="action">Hello Button</my-button>',
+            methods: { action: action('clicked') }
+        };
+    })
+    .add('with JSX', () => {
+        return {
+            components: { MyButton },
+            render() {
+                return <my-button onClick={this.action}>With JSX</my-button>;
+            },
+            methods: { action: linkTo('Button', 'with some emoji') }
+        };
+    })
+    .add('with some emoji', () => {
+        return {
+            components: { MyButton },
+            template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+            methods: { action: action('clicked') }
+        };
+    });
